43159141cb7b7907835d6486a95e712ec51949a3
Faith <adegold71@gmail.com>
2018-10-06 11:23:56 +0100

    setup eslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import AppComponent from './app/AppComponent.jsx';
+import AppComponent from './app/AppComponent';
 
 render(
   <AppComponent />,
-  window.document.getElementById('app-container')
+  window.document.getElementById('app-container'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.html$/,
         use: {
-        loader: "html-loader"
-        }
-      }
-    ]
+          loader: 'html-loader',
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    })
-  ]
+      template: './src/index.html',
+    }),
+  ],
 };
